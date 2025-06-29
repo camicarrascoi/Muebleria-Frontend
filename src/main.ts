244ceffee73406224b093f4a6d4b5b88180eece7
spectro451 <bastyfarias30@gmail.com>
2025-06-29 16:41:01 -0400

    ajustes para el login

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes'; 
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './app/auth/auth.interceptor';
+import { authInterceptor } from './app/auth/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes),
-  provideHttpClient(),
-  {
-    provide: HTTP_INTERCEPTORS,
-    useClass: AuthInterceptor,
-    multi: true
-  }
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 });
