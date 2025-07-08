bb6e11627df9da6652b0232bebfc009cce220e00
camcarrascoi <camcarrascoi@gmail.com>
2025-07-08 18:23:08 -0400

    se devolvio el crear usuario desde el login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,18 @@
 import { Routes } from '@angular/router';
-import { LoginComponent }      from './auth/login/login.component';
-import { dashboardRoutes }     from './dashboard/dashboard-routing.module';
+import { LoginComponent } from './auth/login/login.component';
+import { dashboardRoutes } from './dashboard/dashboard-routing.module';
+import { UsuarioComponent } from './dashboard/usuario/usuario.component'; //Importa el componente
 
 export const routes: Routes = [
-  { path: '',        redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login',   component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
 
-  // Ya no existe: { path: 'usuario', component: UsuarioComponent },
+  //Ruta para crear cuenta (visible desde login)
+  { path: 'usuario', component: UsuarioComponent },
 
-  // Todas las rutas de /dashboard, incluyendo usuarios/crear con AdminGuard
+  // Rutas del dashboard
   ...dashboardRoutes,
 
-  // Cualquier otra URL, redirige a login
+  // Redirección por defecto
   { path: '**', redirectTo: 'login' }
 ];
